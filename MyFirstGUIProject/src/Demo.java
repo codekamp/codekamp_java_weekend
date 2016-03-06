1fd498420f1a0791e04b6d01e6d67cada7b3a5b0
Prashant Chaudhary <101.prashant@gmail.com>
2016-03-06 11:45:01 +0530

    button click added

--- MyFirstGUIProject/src/Demo.java
+++ MyFirstGUIProject/src/Demo.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by cerebro on 05/03/16.
  */
-public class Demo {
+public class Demo implements ActionListener {
+
+    public static JButton button1;
+    public static JButton button2;
 
     public static void main(String[] args) {
 
@@ -16,31 +21,54 @@ public class Demo {
         Dimension dimension1 = new Dimension(800, 600);
         panel1.setPreferredSize(dimension1);
         frame1.setContentPane(panel1);
-        panel1.setBackground(Color.blue);
+        panel1.setBackground(Color.BLUE);
 
 
         JPanel panel2 = new JPanel();
-        panel2.setPreferredSize(new Dimension(50, 600));
-        panel2.setBackground(Color.red);
+        panel2.setPreferredSize(new Dimension(50, 590));
+        panel2.setBackground(Color.RED);
 
         JPanel panel3 = new JPanel();
-        panel3.setPreferredSize(new Dimension(150, 600));
-        panel3.setBackground(Color.green);
+        panel3.setPreferredSize(new Dimension(150, 590));
+        panel3.setBackground(Color.GREEN);
 
         JPanel panel4 = new JPanel();
-        panel4.setPreferredSize(new Dimension(580, 600));
-        panel4.setBackground(Color.yellow);
+        panel4.setPreferredSize(new Dimension(580, 590));
+        panel4.setBackground(Color.YELLOW);
 
 
         panel1.add(panel2);
         panel1.add(panel3);
         panel1.add(panel4);
 
-        JButton button1 = new JButton("Register at CodeKamp!");
+        Demo.button1 = new JButton("Register at CodeKamp!");
+        panel4.add(Demo.button1);
+        Demo.button1.setForeground(Color.RED);
 
-        panel4.add(button1);
+        Demo demo1 = new Demo();
+        Demo.button1.addActionListener(demo1);
+
+        Demo.button2 = new JButton("Contact Us");
+        panel4.add(Demo.button2);
+        Demo.button2.addActionListener(demo1);
+
+        JLabel label1 = new JLabel("Hi");
+        panel2.add(label1);
+
+        JTextField field1 = new JTextField("hello");
+        panel3.add(field1);
+
 
         frame1.pack();
         frame1.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == Demo.button1) {
+            System.out.println("Register at CodeKamp button clicked");
+        } else if(e.getSource() == Demo.button2) {
+            System.out.println("Contact Us button clicked");
+        }
     }
 }
